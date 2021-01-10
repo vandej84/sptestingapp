bf768adc67b68a1667670c47cdbbfd315d5af9ce
Jeremiah V <vandej84@ferris.edu>
2021-01-10 14:30:46 -0500

    MyDorm switchover

--- src/App.js
+++ src/App.js
@@ -11,83 +11,97 @@ function App() {
   const [notes, setNotes] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
 
-  useEffect(() => {
-    fetchNotes();
-  }, []);
-
-async function fetchNotes() {
-  const apiData = await API.graphql({ query: listNotes });
-  const notesFromAPI = apiData.data.listNotes.items;
-  await Promise.all(notesFromAPI.map(async note => {
-    if (note.image) {
-      const image = await Storage.get(note.image);
-      note.image = image;
-    }
-    return note;
-  }))
-  setNotes(apiData.data.listNotes.items);
-}
-
-async function createNote() {
-  if (!formData.name || !formData.description) return;
-  await API.graphql({ query: createNoteMutation, variables: { input: formData } });
-  if (formData.image) {
-    const image = await Storage.get(formData.image);
-    formData.image = image;
-  }
-  setNotes([ ...notes, formData ]);
-  setFormData(initialFormState);
-}
-
-  async function deleteNote({ id }) {
-    const newNotesArray = notes.filter(note => note.id !== id);
-    setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
-  }
-  
-  async function onChange(e) {
-  if (!e.target.files[0]) return
-  const file = e.target.files[0];
-  setFormData({ ...formData, image: file.name });
-  await Storage.put(file.name, file);
-  fetchNotes();
-}
-
 
   return (
-    <div className="App">
-      <h1>My Notes App</h1>
-      <input
-        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
-        value={formData.name}
-      />
-      <input
-        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Note description"
-        value={formData.description}
-      />
-	  <input
-		  type="file"
-		  onChange={onChange}
-	  />
-      <button onClick={createNote}>Create Note</button>
-      <div style={{marginBottom: 30}}>
-        {
-			notes.map(note => (
-				<div key={note.id || note.name}>
-					<h2>{note.name}</h2>
-					<p>{note.description}</p>
-					<button onClick={() => deleteNote(note)}>Delete note</button>
-				    {
-					  note.image && <img src={note.image} style={{width: 400}} />
-					}
+  <div id="parent">
+	  <header id="banner" role="banner">
+		<div id="heading">
+			<h1 class="site-title">
+			<a class="logo custom-logo" href="/web/home-community" title="Go to home-community">
+			<img alt="home-community" height="61" src="/header_image.png" width="2118" />
+			</a>
+			</h1>
+			<h2 class="page-title">
+			<span tabindex="0">Home</span>
+			</h2>
+		</div>
+		<nav class="sort-pages modify-pages" id="navigation">
+			<h1>
+			<span>Navigation</span>
+			</h1>
+
+			<div class="navigation-gradient">
+			</div>
+
+			<div class="search">
+				<form action="https://myfsu.ferris.edu/web/home-community/1?p_p_auth=zfv5HZO0&amp;p_p_id=77&amp;p_p_lifecycle=0&amp;p_p_state=maximized&amp;p_p_mode=view&amp;_77_struts_action=%2Fjournal_content_search%2Fsearch" class="aui-form" method="post" name="_77_fm" onSubmit="submitForm(this); return false;">
+					<span class="aui-field aui-field-text aui-field-inline lfr-search-keywords">
+						<span class="aui-field-content">
+							<span class="aui-field-element ">
+								<input class="aui-field-input aui-field-input-text"  id="keywords"  name="keywords"    title="Search Web Content" type="text" value="Search&#x2e;&#x2e;&#x2e;&#x2e;&#x2e;"  onBlur="if (this.value == '') { this.value = '\u0053\u0065\u0061\u0072\u0063\u0068\u002e\u002e\u002e\u002e\u002e'; }" onFocus="if (this.value == '\u0053\u0065\u0061\u0072\u0063\u0068\u002e\u002e\u002e\u002e\u002e') { this.value = ''; }" size="30"  />
+							</span>
+						</span>
+					</span>
+					<span class="aui-field aui-field-text aui-field-inline lfr-search-button">
+						<span class="aui-field-content">
+							<span class="aui-field-element ">
+								<input class="aui-field-input aui-field-input-text"  id="search"  name="search"     type="image"   alt="search" src="https://myfsu.ferris.edu/LP5-ellucian-theme/images/common/search.png"  />
+							</span>
+						</span>
+					</span>
+				</form>
+			</div>
+
+			<ul>
+				<li class="selected">
+					<a href="https://myfsu.ferris.edu/web/home-community/1" ><span tabindex="0">Home</span></a>
+				</li>
+				<li>
+					<a href="https://myfsu.ferris.edu/web/home-community/student-hidden-" ><span tabindex="0">Student</span></a>
+				</li>
+				<li>
+					<a href="https://myfsu.ferris.edu/web/home-community/forms" ><span tabindex="0">Forms</span></a>
+				</li>
+				<li>
+					<a href="https://ferris.edu/library" target="_blank"><span tabindex="0">Library</span></a>
+				</li>
+				<li>
+					<a href="https://myfsu.ferris.edu/web/home-community/new-evernt" ><span tabindex="0">Communities</span></a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+		
+	<div class="content">
+		<div class="info">
+			<h1>Ferris State University MyDorm</h1>
+			<p>Use the buttons below to control your dorm room's lights, heat, locks, and more.</p>
+		</div>
+		<div class="controls">
+			<div class="row">
+				<div class="lights col">
+					<h2>Lights</h2>
+					<a href="#" class="button">Turn Lights On</a>
+
+					<a href="#" class="button">Turn Lights Off</a>
 				</div>
-			  ))
-		}
-      </div>
-      <AmplifySignOut />
-    </div>
+				<div class="door col">
+					<h2>Door</h2>
+					<a href="#" class="button">Lock Door</a>
+
+					<a href="#" class="button">Unlock Door</a>
+				</div>
+				<div class="window col">
+					<h2>Window</h2>
+					<a href="#" class="button">Open Window</a>
+
+					<a href="#" class="button">Close Window</a>
+				</div>
+			</div>
+			<iframe src="http://192.168.1.89:3000/d-solo/VVngEiRgk/internet-speeds?orgId=1&from=1608827072206&to=1609431872206&panelId=2" width="450" height="200" frameborder="0"></iframe>
+		</div>
+	</div>
+</div>
   );
 }
 
